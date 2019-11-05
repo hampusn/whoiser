6f33c3a1cb7c94379ec08df0825af4cff5ba5aa7
Hampus Nordin <nordin.hampus@gmail.com>
2019-11-05 21:02:30 +0100

    Updates

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT      = process.env.PORT || 5000;
 
 const http      = require('http');
 const NodeCache = require('node-cache');
-const whois     = require('node-whois');
+const whois     = require('whois');
 const requestIp = require('request-ip');
 
 const cacheTtl  = process.env.CACHE_TTL || 86400;
